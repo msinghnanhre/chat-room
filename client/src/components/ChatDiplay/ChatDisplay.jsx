8a0454bcb8610690bf15b18f6ae5bf74b0045ca6
nathanieltse <nathanieltse@hotmail.com>
2021-06-25 12:41:12 -0400

    fixed display message on both ends

--- client/src/components/ChatDiplay/ChatDisplay.jsx
+++ client/src/components/ChatDiplay/ChatDisplay.jsx
@@ -2,29 +2,21 @@ import backIcon from '../../assets/icons/backIcon.svg'
 import './ChatDisplay.scss'
 
 const ChatDisplay = (props) => {
-    // console.log(props)
+    console.log(props.chat)
     return(
         <section className="display-box">
             <button className="display-box__back-btn"><img className="display-box__back-icon" src={backIcon} alt="sending icon"/></button>
             <h2 className="display-box__title">
                 Chatroom
             </h2>
-            {/* needs styling */}
-            <h5>{props.welcome}</h5>
-            
-            {/* {props.myMessages.map((message,index) => {
-            return (
-                <div className="display-box__message-right" key={index}>
-                    <span className="display-box__message-name">Me</span>
-                    <h4 className="display-box__message-text">{message}</h4>
-                </div>
-            )
-            })} */}
+
+            <h5 className="display-box__">{props.welcome}</h5>
+
             {props.chat.map((payload, index) => {
             return (
                 <div className={props.match.params.username === payload.name ? "display-box__message-right" : "display-box__message-left" } key={index}>
-                    <span className="display-box__message-name"> {payload.name}</span>
-                    <h4 className="display-box__message-text"> {payload.message}</h4>
+                    <span className="display-box__message-name"> {props.match.params.username === payload.name ?  "Me" : payload.username }</span>
+                    <h4 className="display-box__message-text"> {props.match.params.username === payload.name ?  payload.message : payload.userMessage }</h4>
                 </div> 
             )
             })}
